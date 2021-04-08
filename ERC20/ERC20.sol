fda57ecc1320ff4af34ec5e4cb20c3edc07364a9
ILoomans <50031945+ILoomans@users.noreply.github.com>
2021-04-08 19:59:21 +0100

    Update ERC20.sol

--- ERC20/ERC20.sol
+++ ERC20/ERC20.sol
@@ -105,7 +105,7 @@ contract ERC20 is Context, IERC20 {
      * - `recipient` cannot be the zero address.
      * - the caller must have a balance of at least `amount`.
      */
-    function transfer(address recipient, uint256 amount) public returns (bool) {
+    function transfer(address recipient, uint256 amount) internal returns (bool) {
         _transfer(_owner, recipient, amount);
         return true;
     }
